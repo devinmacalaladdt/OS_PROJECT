c09c7683518f2a2d13e1ab8876ec5a808b918f31
David Gasperini <dlg4242@live.com>
2020-02-08 19:19:46 -0500

    syscall Part 3

--- OS Project 1/syscall.c
+++ OS Project 1/syscall.c
@@ -1,11 +1,11 @@
 /* syscall.c
  *
  * Group Members Names and NetIDs:
- *   1.
- *   2.
+ *   1. Devin Malacalad (dtm)
+ *   2. David Gasperini (dlg195)
  *
  * ILab Machine Tested on:
- *
+ * grep.cs.rutgers.edu
  */
 
 #include <stdio.h>
@@ -17,12 +17,20 @@ double avg_time = 0;
 
 int main(int argc, char *argv[]) {
 
-	/* Implement Code Here */
+    /* Implement Code Here */
+	struct timeval start;
+	gettimeofday(&start, NULL);
+	int x;
+	for (x = 0; x < 5000000; x++) {
+		syscall(SYS_getuid);//no need to store result because it's just a time test
+	}
+	struct timeval end;
+	gettimeofday(&end, NULL);
+	avg_time = (-(start.tv_sec + start.tv_usec) + (end.tv_sec + end.tv_usec)) / (5000000.0);
+    // Remember to place your final calculated average time
+    // per system call into the avg_time variable
 
-	// Remember to place your final calculated average time
-	// per system call into the avg_time variable
+    printf("Average time per system call is %f microseconds\n", avg_time);
 
-	printf("Average time per system call is %f microseconds\n", avg_time);
-
-	return 0;
+    return 0;
 }
