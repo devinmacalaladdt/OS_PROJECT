1d8f5d68d2de08d637adb771ce9d40caaa64ffb1
David Gasperini <dlg4242@live.com>
2020-03-11 00:35:22 -0400

    test file

--- OS Project 2/code/benchmarks/test.c
+++ OS Project 2/code/benchmarks/test.c
@@ -10,6 +10,9 @@
  * You can modify and use this program as much as possible.
  * This will not be graded.
  */
+
+rpthread_mutex_t mutex;
+
 void* foo(void * args){
 
 	int x = *((int *)args);
@@ -55,9 +58,28 @@ void * yeet(void * args){
 	rpthread_exit((void *)ret);
 
 }
-int main(int argc, char **argv) {
 
-	rpthread_t thread_id; 
+int counter = 0;
+void * m_test(void * args) {
+
+	rpthread_mutex_lock(&mutex);
+
+	unsigned long i = 0;
+	counter += 1;
+	printf("\n Job %d started\n", counter);
+
+	for (i = 0; i < (0xFFFFFFFF); i++);
+	printf("\n Job %d finished\n", counter);
+
+	rpthread_mutex_unlock(&mutex);
+
+	rpthread_exit(NULL);
+}
+
+int main(int argc, char **argv) {
+	rpthread_mutex_init(&mutex, NULL);
+	printf("Mutex Initialized\n");
+	/*rpthread_t thread_id; 
 	rpthread_t thread_id2;
 	rpthread_t thread_id3;
 	void * p = malloc(sizeof(int));
@@ -83,7 +105,14 @@ int main(int argc, char **argv) {
 
     free(p);
     free(p2);
-    free(p3);
+    free(p3);*/
+	rpthread_t t1, t2;
+	rpthread_create(&t1, NULL, m_test, NULL);
+	rpthread_create(&t1, NULL, m_test, NULL);
+	rpthread_join(t1, NULL);
+	rpthread_join(t2, NULL);
+	printf("Destroy\n");
+	rpthread_mutex_destroy(&mutex);
 
 	return 0;
 }
